f02d8e7ac06fe64ccdd4e05d72e3092125641382
randolphpark <randolph.park@gmail.com>
2019-01-22 12:39:04 +1000

    Update with CURD User

--- react-admin-fe/src/App.js
+++ react-admin-fe/src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import { Admin, Resource } from 'react-admin';
-import { UserList, UserCreate } from './users';
-import { PostList, PostEdit, PostCreate, PostShow } from './posts';
 import buildPrismaProvider from 'ra-data-prisma';
-import UserIcon from '@material-ui/icons/Group';
-import PostIcon from '@material-ui/icons/Book';
+import { UserList, UserCreate, UserEdit, UserShow, AccountsList, AccountsCreate } from './users';
 
 class App extends Component {
     constructor() {
         super();
         this.state = { dataProvider: null };
     }
+
     componentDidMount() {
         buildPrismaProvider({ clientOptions: { uri: 'http://localhost:4466' } })
             .then(dataProvider => this.setState({ dataProvider }));
@@ -23,24 +21,23 @@ class App extends Component {
             return <div>Loading</div>;
         }
 
+        console.log(dataProvider)
         return (
             <Admin
                 dataProvider={dataProvider}
                 title="Prisma Admin"
             >
                 <Resource
-                    name="Post"
-                    icon={PostIcon}
-                    list={PostList}
-                    edit={PostEdit}
-                    create={PostCreate}
-                    show={PostShow}
+                    name="Account"
+                    list={AccountsList}
+                    create={AccountsCreate}
                 />
                 <Resource
                     name="User"
                     list={UserList}
                     create={UserCreate}
-                    icon={UserIcon}
+                    show={UserShow}
+                    edit={UserEdit}
                 />
             </Admin>
         );
@@ -48,3 +45,4 @@ class App extends Component {
 }
 
 export default App;
+
--- react-admin-fe/src/users.js
+++ react-admin-fe/src/users.js
@@ -7,13 +7,23 @@ import {
   SimpleForm,
   SimpleList,
   TextField,
-  TextInput
+  TextInput,
+  Show,
+  Edit,
+  SimpleShowLayout,
+  EditButton,
 } from "react-admin";
 
 export const UserCreate = props => (
   <Create {...props}>
     <SimpleForm>
-      <TextInput source="name" />
+
+      <TextInput source="first_name" />
+      <TextInput source="last_name" />
+      <TextInput source="auth0_id" />
+      <TextInput source="email" />
+      <TextInput source="inserted_at" />
+      <TextInput source="updated_at" />
     </SimpleForm>
   </Create>
 );
@@ -21,13 +31,71 @@ export const UserCreate = props => (
 export const UserList = props => (
   <List title="All users" {...props}>
     <Responsive
-      small={<SimpleList primaryText={record => record.name} />}
-      medium={
+      small={
         <Datagrid>
-          <TextField source="name" />
+          <TextField source="first_name" />
+          <TextField source="last_name" />
+          <TextField source="auth0_id" />
+          <TextField source="email" />
+          <TextField source="inserted_at" />
+          <TextField source="updated_at" />
           <TextField source="id" />
+          <EditButton basePath="/user" />
         </Datagrid>
       }
     />
   </List>
 );
+
+export const UserShow = props => (
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="first_name" />
+      <TextField source="last_name" />
+      <TextField source="auth0_id" />
+      <TextField source="email" />
+      <TextField source="inserted_at" />
+      <TextField source="updated_at" />
+    </SimpleShowLayout>
+  </Show>
+);
+
+const UserTitle = ({ record }) => {
+  return <span>User {record ? `"${record.first_name}"` : ''}</span>;
+};
+
+export const UserEdit = props => (
+  <Edit title={<UserTitle />} {...props}>
+    <SimpleForm>
+      <TextInput source="first_name" />
+      <TextInput source="last_name" />
+      <TextInput source="auth0_id" />
+      <TextInput source="email" />
+      <TextInput source="inserted_at" />
+      <TextInput source="updated_at" />
+    </SimpleForm>
+  </Edit>
+);
+
+
+export const AccountsCreate = props => {
+  return <Create {...props}>
+    <SimpleForm>
+      <TextInput source="name" />
+    </SimpleForm>
+  </Create>
+};
+
+export const AccountsList = props => {
+
+  return <List title="All accountses" {...props}>
+    <Responsive
+      small={<SimpleList primaryText={record => record.name} />}
+      medium={
+        <Datagrid>
+          <TextField source="name" />
+        </Datagrid>
+      }
+    />
+  </List>
+};
